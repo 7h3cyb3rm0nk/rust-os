b475fb1d6ae580757d5ce79e79f9812f55050e0a
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-12-31 21:03:27 +0530

    add: add comments for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the rust sta library
+#![no_main]// disable all rust entry points
+
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle this function name
 pub extern "C" fn _start() -> ! {
+    //this function is the entry point since the 
+    //linker looks for a function _start() by default
     loop{}
 }
 
 #[panic_handler]
+//this function is called on panic
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
