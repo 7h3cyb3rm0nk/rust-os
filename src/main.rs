3efbb0091e0740634166292ba27ff336a7512abd
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-12-31 23:23:38 +0530

    feat: hello world to vga buffer add code to write hello world to vga buffer

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,22 @@
 #![no_main]// disable all rust entry points
 
 use core::panic::PanicInfo;
-
+static HELLO :&[u8] = b"HELLO WORLD";
 #[no_mangle] // don't mangle this function name
 pub extern "C" fn _start() -> ! {
+
     //this function is the entry point since the 
     //linker looks for a function _start() by default
+    //
+
+    let vga_buffer = 0xb8000 as *mut u8; // declares a raw pointer for the vga_buffer
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize *2) = byte; // writes each byte HELLO to vga buffer
+            *vga_buffer.offset(i as isize *2 + 1) = 0xb; // writes to the attribute section for
+                                                         // each character in vga buffer
+        }
+    }
     loop{}
 }
 
