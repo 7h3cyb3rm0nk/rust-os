f607ff54bed01d5eb25aa82d0009da8ccdd26d4c
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-01-20 17:28:28 +0530

    fix: delete comments/unused codes

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,6 @@ mod vga_buffer;
              //
 pub extern "C" fn _start() -> ! {
 
-    //this function is the entry point since the 
-    //linker looks for a function _start() by default
-    //
-    //
-    // let vga_buffer = 0xb8000 as *mut u8; // declares a raw pointer for the vga_buffer
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize *2) = byte; // writes each byte HELLO to vga buffer
-    //         *vga_buffer.offset(i as isize *2 + 1) = 0xb; // writes to the attribute section for
-    //                                                      // each character in vga buffer
-    //     }
-    // }
-    //
-    // vga_buffer::WRITER.lock().write_string("Hello World");
-    // write!(vga_buffer::WRITER.lock(), "Rust Os").unwrap();
-    //
-    
-    //
     println!("RUST-OS VERSION 0.1.0 ");
     #[cfg(test)]
     test_main();
