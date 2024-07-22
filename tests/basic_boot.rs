5b61ac14a4ce9c0ca81d43f20b413b65832e5ba3
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-07-23 02:36:54 +0530

    add(tests/basic_boot.rs): use println macro from crate

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -3,25 +3,22 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
+use rust_os::println;
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     test_main();
-    loop{}
+    loop {}
 }
-
 
 #[test_case]
 fn test_println() {
-
     println!("test_println output");
 }
 
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
